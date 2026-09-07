169548df623406bdb1a89ab06c6e64e6db89eae8
johan0920-sketch <johan0920@gmail.com>
2026-09-07 16:09:05 -0500

    Parcial I

---
+++ Parcial-Uno - Nivel 0/src/App.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+import service.ServiceCuenta;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        ServiceCuenta serviceCuenta=new ServiceCuenta();
+        //Crear el menu aqui e invocar los metodos de la clase ServiceCuenta
+        //implementar todos los metodos de la interface IServiceCuenta
+    }
+}
